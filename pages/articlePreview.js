24dbf88061c66ea7c6971ed4ead730e61c140af6
Felix <felixrunye@gmail.com>
2021-09-17 14:16:23 +0300

    updated article preview page

--- pages/articlePreview.js
+++ pages/articlePreview.js
@@ -2,13 +2,13 @@ import Head from 'next/head';
 
 function articlePreview() {
     return (
-        <div className=" bg-gray-300 h-screen px-8 pt-8">
+        <div className=" bg-gray-300 h-screen px-8 pt-8 w-full">
             <Head>
                 <title>Article Preview Challenge</title>
             </Head>
-            <div className="mx-auto">
-                <img src="/images/drawers.jpeg" className="rounded-tr-md rounded-tl-md h-48 w-full" alt="" />
-                <div className="bg-white p-8 rounded-bl-md rounded-br-md">
+            <div className="mx-auto md:w-3/5 md:flex md:h-auto">
+                <img src="/images/drawers.jpeg" className="rounded-tr-md rounded-tl-md h-48 w-full md:h-auto md:w-2/5 md:rounded-bl-md md:rounded-tl-md " alt="" />
+                <div className="bg-white p-8 rounded-bl-md rounded-br-md rounded-tr-md md:rounded-bl-none ">
                     <h2 className="text-gray-700 font-semibold">Shift overall look and feel by adding these wonderful touches to furniture in your home</h2>
                     <p className="text-sm text-gray-600 mt-4"> Ever been in a room and felt like something was missing? perhaps it felt slightly bare 
                         and uninviting. iv got some simple tips to help you make room feel complete. </p>
